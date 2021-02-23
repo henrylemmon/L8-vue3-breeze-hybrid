1d152fffa1397022823c8a50a80e381ccd9ede15
henrylemmon <henrylemmon@yahoo.com>
2021-02-22 17:56:52 -0800

    changed order of buttons in footer of login.blade

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -33,9 +33,9 @@
                 </div>
 
                 <div class="flex justify-between items-baseline mt-6">
-                    <a href="/" class="bg-blue-500 py-2 px-3 text-white rounded-lg hover:bg-blue-600">{{ __('Cancel') }}</a>
-                    <a href="{{ route('password.request') }}" class="text-blue-500 hover:text-blue-600 hover:underline">{{ __('Forgot Your Password?') }}</a>
                     <button type="submit" class="bg-blue-500 py-2 px-3 text-white rounded-lg hover:bg-blue-600">{{ __('Submit') }}</button>
+                    <a href="{{ route('password.request') }}" class="text-blue-500 hover:text-blue-600 hover:underline">{{ __('Forgot Your Password?') }}</a>
+                    <a href="/" class="bg-blue-500 py-2 px-3 text-white rounded-lg hover:bg-blue-600">{{ __('Cancel') }}</a>
                 </div>
             </form>
         </div>
